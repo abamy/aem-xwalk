b6cb468bd727dcc83efa80ae16cb4a7b00b21545
Arthur Bamy <arthur.bamy@gmail.com>
2024-02-13 13:45:44 +0100

    adapt header footer for dev

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -93,9 +93,8 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
 export default async function decorate(block) {
   // fetch nav content
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : `${getSiteRoot()}/nav`;
+  const navPath = navMeta ? new URL(navMeta).pathname : `${getSiteRoot()}nav`;
   const resp = await fetch(`${navPath}.plain.html`);
-
   if (resp.ok) {
     const html = await resp.text();
 
